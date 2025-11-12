9f48e71ee0162402d606fd131493871d45a0273d
Jnanesh2005 <jnaneshmj2005@gmail.com>
2025-11-12 21:08:34 +0530

    Add .gitignore and remove venv from tracking

--- rakshara_proj/urls.py
+++ rakshara_proj/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from accounts.views import set_language
 from django.conf import settings               # <-- 1. IMPORT
 from django.conf.urls.static import static     # <-- 2. IMPORT
 
@@ -9,5 +8,4 @@ urlpatterns = [
     path('', include('accounts.urls')),  # ✅ this ensures '/' goes to home()
     path('classroom/', include('classroom.urls')),
     path('health/', include(('health.urls', 'health'), namespace='health')),
-    path('set-language/', set_language, name='set_language'),
 ]
